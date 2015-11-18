dcdd974f6eb21d24e9321a598c9dfa3bcd568359
userimack <mahendra.k12@gmail.com>
2015-11-19 00:17:05 +0530

    bug fixed for reboot

--- one_nvidia.sh
+++ one_nvidia.sh
@@ -8,18 +8,18 @@ echo "Enter the root user password::"
 
 echo "Running command 'mv /boot/initramfs-$(uname -r).img /boot/initramfs-$(uname -r)-nouveau.img': "
 
-#echo `sudo mv /boot/initramfs-$(uname -r).img /boot/initramfs-$(uname -r)-nouveau.img`
+echo `sudo mv /boot/initramfs-$(uname -r).img /boot/initramfs-$(uname -r)-nouveau.img`
 
 echo "Running command 'dracut --omit-drivers nouveau /boot/initramfs-$(uname -r).img $(uname -r)'"
-#echo `sudo dracut --omit-drivers nouveau /boot/initramfs-$(uname -r).img $(uname -r)`
+echo `sudo dracut --omit-drivers nouveau /boot/initramfs-$(uname -r).img $(uname -r)`
 
 echo "Reboot required"
 echo -e "Press y to reboot else n to terminate reboot"
 read ans
 
-if [ $ans == 'y'] ; then
-    echo "in"
-    #echo `reboot`
+if [ $ans == y ] ; then
+    echo `reboot`
 else
     echo "You have presses n : Terminated"
 
+fi
